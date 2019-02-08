0fa5947d6514adce34b9de00a87b626647bfe705
it-arjan <it-arjan@users.noreply.github.com>
2019-02-08 15:32:57 +0100

    settings: add tabs, set active tab

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { BackendService } from 'src/app/services/backend.service';
 import { ConfigSetting } from 'src/app/models/configsetting.model';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
@@ -22,42 +22,54 @@ export class SettingsComponent implements OnInit {
   loggedOn = false //get from route?@!??!
   changepwdClicked=false
   capsLock=false
+  @ViewChild('tabset') tabset
+
   ngOnInit() {
-    this.initForms()
-    this.readConfig()
-    }
-    initForms(){
-      this.logonForm = new FormGroup({
-        'password': new FormControl('',[Validators.required])        
-      })
-      this.changePwdForm = new FormGroup({
-        'oldpassword': new FormControl('',[Validators.required]),
-        'newpassword': new FormControl('',[Validators.required]),       
-        'newpassword2': new FormControl('',[Validators.required]),       
-      })
-    }
+  this.initForms()
+  this.readConfig()
   
-  readConfig(){
-    this.settings.length=0 //to trigger short screen feedback
-    this._bs.readConfig()
-      .then((result)=>{
-        this.settings = result
-        //console.log(this.settings)
-        })
+  }
+  tabs=['logon','changepwd', 'settings','logs']
+  activetabNr=2
+
+  getActiveTab(){
+    if (this.activetabNr > this.tabs.length -1)
+      this._ui.error(`kan active tab nr ${this.activetabNr} niet automatisch selecteren, klik zelf even of start opnieuw op.`)
+    return this.tabs[this.activetabNr]
   }
 
-  writeConfig(){
-    //this._cfg.changePassword();
-    this._bs.writeConfig(this.settings)
-  }
-
-  checkCapslock(event){
-    this.capsLock=event.getModifierState("CapsLock")
-    if (this.capsLock){
-      this._ui.error('Caps Lock is on!!')
+  setActiveTab(){
+    let name
+    if ( name = this.searchIncorrectSetting()) {
+      this._ui.error(`Some settings (${name}) show errors, please correct them before logging on`)
+      this.activetabNr=2
     }
+    else if (!this.loggedOn) this.activetabNr=0
   }
 
+  searchIncorrectSetting(): string {
+    console.log('searchIncorrectSetting')
+    for (let setting of this.settings){
+      console.log(setting.error)
+      if (setting.error)  {
+        console.log('Found Error setting: ' + setting.name)
+        return setting.name
+      }
+    }
+    return undefined;
+  }
+
+  initForms(){
+    this.logonForm = new FormGroup({
+      'password': new FormControl('',[Validators.required])        
+    })
+    this.changePwdForm = new FormGroup({
+      'oldpassword': new FormControl('',[Validators.required]),
+      'newpassword': new FormControl('',[Validators.required]),       
+      'newpassword2': new FormControl('',[Validators.required]),       
+    })
+  }
+ 
   onLogon(){
     console.log( 'setings component: sending pwd ' + this.logonForm.get('password').value)
     this._bs.logOn(this.logonForm.get('password').value)
@@ -80,13 +92,35 @@ export class SettingsComponent implements OnInit {
         if (new1.length < 6) this._ui.error("nieuw wachtwoord moet minimaal 6 karakters lang")
         else if (new1 != new2) this._ui.error("nieuwe wachtwoorden niet hetzelfde")
         else {
-          this._bs.changePassword(oldpass, new1).then(()=>{
-            this.changepwdClicked=!this.changepwdClicked
+          this._bs.changePassword(oldpass, new1)
+          .then(()=>{
+            this.changePwdForm.setValue({oldpassword:'', newpassword:'', newpassword2:''})
             this._ui.success()
           })
           .catch(x=> this._ui.error("Error writing config."))
         }
       })
       .catch(()=> this._ui.error("oude wachtwoord helaas niet goed"))
+  }
+
+  readConfig(){
+    this.settings.length=0 //to trigger short screen feedback
+    this._bs.readConfig()
+      .then((result)=>{
+        this.settings = result
+        this.setActiveTab()
+        })
+  }
+
+  writeConfig(){
+    //this._cfg.changePassword();
+    this._bs.writeConfig(this.settings)
+  }
+
+  checkCapslock(event){
+    this.capsLock=event.getModifierState("CapsLock")
+    if (this.capsLock){
+      this._ui.error('Caps Lock is on!!')
+    }
   }
 }
