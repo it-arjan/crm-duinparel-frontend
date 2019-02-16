e2d5fb92b6718ced2d531aca627f4991c3cd7cdd
it-arjan <it-arjan@users.noreply.github.com>
2019-02-16 22:30:18 +0100

    cust-list: dubbelklik zoekt op email

--- src/app/components/booking/customer-list/customer-list.component.ts
+++ src/app/components/booking/customer-list/customer-list.component.ts
@@ -9,16 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
-  customers : Array<Customer>;
-  selectedIdx:number;
+  customers : Array<Customer>
+  selectedIdx:number
   constructor(private _router: Router, private _dataService: DataService) { }
 
   ngOnInit() {
-    this.customers = this._dataService.searchResult;
+    this.customers = this._dataService.searchResult
   }
   onClick(idx:number, action:string){
     this.selectedIdx=idx;
     if (action==='book') this._router.navigate(['booking','cust',this.customers[this.selectedIdx].id, 'bookings'])
     else this._router.navigate(['booking','cust',this.customers[this.selectedIdx].id])
+  }
+  onDblclick(idx: number){
+    this._dataService.searchCustomers(this.customers[idx].email)
   }
 }
