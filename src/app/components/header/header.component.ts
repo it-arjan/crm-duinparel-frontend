ecdc5bae3834c8f25da732f70cda80ca9771d2ef
it-arjan <it-arjan@users.noreply.github.com>
2019-02-23 16:14:22 +0100

    add msghistory in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,7 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked, OnDestroy, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, OnDestroy, OnChanges, SimpleChanges, ChangeDetectorRef, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { UIService } from 'src/app/services/ui.service';
 import { UserFeedback } from 'src/app/models/UserFeedback.model';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
-
+import {trigger, state, style, animate, transition} from '@angular/animations';
 import { IconFeedback } from 'src/app/models/icon-feedback';
 import { MessageFeedback } from 'src/app/models/message-feedback';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -17,6 +10,8 @@ import { take } from 'rxjs/operators';
 import { DataService } from 'src/app/services/data.service';
 import { Router } from '@angular/router';
 import { BackendService } from 'src/app/services/backend.service';
+import { tDataResult } from 'src/app/services/interfaces.persist';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -36,51 +31,74 @@ export class HeaderComponentComponent implements OnInit, OnDestroy {
   constructor(
     private _ui : UIService, private _modalService: NgbModal,
     private _bs: BackendService, private _cd: ChangeDetectorRef,
-    private _ds: DataService, private _router: Router
+    private _auth: AuthService,
+    private _ds: DataService, private _r2: Renderer2
     ) { }
-  notificationState:string;
+  notificationState:string
 
-  iconFeedback:IconFeedback;
+  iconFeedback:IconFeedback
   showIconFeedback: boolean
   
-  msgFeedback:MessageFeedback;
+  msgFeedback:MessageFeedback
   showMsgFeedback: boolean
-  msgType:string;
-  
+  msgType:string
+
+  mfList:MessageFeedback[]=[]
+  @ViewChild('notifications') notifRef: ElementRef
+  notifClicked=false
+
   resetAnimation(){
      this.notificationState = 'in'
      setTimeout(()=>{ this.notificationState = 'out' },1000)
   }
+
   ngOnInit() {
     console.log('ngOnInit ')
     this._ui.notifier()
-      .subscribe((msg:UserFeedback)=>{
-        this.notificationState = 'in'
-        this.processFeedback(msg)
-        //Trigger state change after view is rendered
-        setTimeout(()=>{ this.notificationState = 'out' },1000)
-        this._cd.detectChanges()
+      .subscribe((feedback:UserFeedback)=>{
+        this.onUINotification(feedback)
     })
- 
-    //TODO messages doesn't display
-    //something seems to freeze in angular change detection
+
     this._ds.dataReadyReplay().pipe(take(1)) //auto-unsubscribe
       .subscribe((result)=>{
-          if (result.error) { 
-              console.log('Fout bij ophalen data!' + result.error)
-              console.log(result)
-              this._ui.error('Fout bij ophalen data: '+ result.error )// 
-          } else {
-            this._ui.info('de database is beschikbaar' )
-            console.log('HeaderComponentComponent: data ophalen success!')
-          }
+        this.onDataReady(result)
     })    
+  }
+
+  onUINotification(feedback:UserFeedback){
+    if (this._auth.isAuthenticated){
+      this.notificationState = 'in'
+      this.processFeedback(feedback)
+      //Trigger state change after view is rendered
+      setTimeout(()=>{ this.notificationState = 'out' },1000)
+      this._cd.detectChanges() //hack to pickup the first message
+    }
+  }
+
+  onClickNotifications(){
+    this.notifClicked=!this.notifClicked
+
+    let action = this.notifClicked ? 'block': 'none'
+    console.log('action='+action)
+    //this._r2[action](this.notifRef.nativeElement, 'open-notifications')
+    this._r2.setStyle(this.notifRef.nativeElement,'display', action)
+  }
+  
+  onDataReady(result: tDataResult){
+    if (result.error) { 
+        console.log('Fout bij ophalen data!' + result.error)
+        console.log(result)
+        this._ui.error('Fout bij ophalen data: '+ result.error )// 
+    } else {
+      //this._ui.info('de database is beschikbaar' )
+      console.log('handleDataReady: data ophalen success!')
+    }
   }
   // ngDoCheck(){
   //   console.log('=-=-=-=-=-=-=-=-=-=-=- DoCheck EXPENSIVE change detection in header  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
   //   console.log()
   // }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this._ui.notifier().unsubscribe()
     //this._ds.dataReady().unsubscribe()
   }
@@ -109,8 +127,12 @@ export class HeaderComponentComponent implements OnInit, OnDestroy {
         this.showMsgFeedback=true
         this.msgFeedback=new MessageFeedback('danger',feedback.message);
         break;
-    } 
+    }
+    // Start adding msgs to history after logon
+    if (this._auth.isAuthenticated() && this.msgFeedback) 
+      this.mfList.unshift(this.msgFeedback) 
   }
+
   exitElectron(){ //todo make method on service 
     const modalRef = this._modalService.open(ModalConfirmComponent);
     modalRef.componentInstance.title = 'Programma afsluiten';
